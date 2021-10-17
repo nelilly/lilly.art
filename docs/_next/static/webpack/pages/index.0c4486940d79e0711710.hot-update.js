27e71c3af15fd8d75234fb8203282083c37db6bc
Nathan Lilly <nathan_lilly@comcast.com>
2021-10-17 14:34:07 -0400

    Deploy docs

---
+++ docs/_next/static/webpack/pages/index.0c4486940d79e0711710.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate_N_E("pages/index",{
+
+/***/ "./src/data/juvenilia.json":
+/*!*********************************!*\
+  !*** ./src/data/juvenilia.json ***!
+  \*********************************/
+/*! exports provided: artist, designer, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"artist\\\":[{\\\"title\\\":\\\"TITLE\\\",\\\"url\\\":\\\"/images/artist/juvenilia/\\\",\\\"description\\\":\\\"\\\"},{\\\"title\\\":\\\"TITLE\\\",\\\"url\\\":\\\"/images/artist/juvenilia/\\\",\\\"description\\\":\\\"\\\"},{\\\"title\\\":\\\"TITLE\\\",\\\"url\\\":\\\"/images/artist/juvenilia/\\\",\\\"description\\\":\\\"\\\"},{\\\"title\\\":\\\"TITLE\\\",\\\"url\\\":\\\"/images/artist/juvenilia/\\\",\\\"description\\\":\\\"\\\"},{\\\"title\\\":\\\"TITLE\\\",\\\"url\\\":\\\"/images/artist/juvenilia/\\\",\\\"description\\\":\\\"\\\"}],\\\"designer\\\":[{\\\"title\\\":\\\"Butterfly Award (front)\\\",\\\"url\\\":\\\"/images/design/juvenilia/butterfly-award-00.jpg\\\",\\\"description\\\":\\\"\\\"},{\\\"title\\\":\\\"Butterfly Award (photo)\\\",\\\"url\\\":\\\"/images/design/juvenilia/butterfly-award-01.jpg\\\",\\\"description\\\":\\\"\\\"},{\\\"title\\\":\\\"Study of Contrast\\\",\\\"url\\\":\\\"/images/design/juvenilia/contrast.jpg\\\",\\\"description\\\":\\\"\\\"},{\\\"title\\\":\\\"Fire Can Be Prevented\\\",\\\"url\\\":\\\"/images/design/juvenilia/fire-can-be-prevented.jpg\\\",\\\"description\\\":\\\"\\\"},{\\\"title\\\":\\\"Fools Play With Fire Sketch\\\",\\\"url\\\":\\\"/images/design/juvenilia/fools-play-with-fire.jpg\\\",\\\"description\\\":\\\"\\\"}]}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3NyYy9kYXRhL2p1dmVuaWxpYS5qc29uLmpzIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/data/juvenilia.json\n");
+
+/***/ })
+
+})

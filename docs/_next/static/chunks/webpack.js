d3610ce90631b39e1ab58c7a5563347cf90f4f83
Nathan E. Lilly <nathan@lilly.net>
2022-08-20 08:40:49 -0400

    Deploy docs

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a80b3a65ac1bdc3a08f3";
+/******/ 	var hotCurrentHash = "62e035a89d2a3bf310b1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

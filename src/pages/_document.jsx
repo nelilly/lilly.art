bca6a22e67ceeef4bf782d01a70ef0c873b9e6be
Nathan E. Lilly <nathan@lilly.net>
2022-08-20 08:40:42 -0400

    Update a11y and minor fixes

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -13,7 +13,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
           <meta httpEquiv="Content-Type" content="text/html" />
